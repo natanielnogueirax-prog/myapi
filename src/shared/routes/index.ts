f3c6e80b30c0b67112564615aaf16884d71821d7
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-15 20:42:41 -0300

    add roles routes

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,11 +1,12 @@
-import { AppError } from '../erros/AppError';
 import { Router } from 'express';
+import { rolesRouter } from '../../roles/http/routes/roles.routes';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  throw new Error('Erro acesso negado');
   return response.json({ message: 'Hello World!' });
 });
+
+routes.use('/roles', rolesRouter);
 
 export { routes };
