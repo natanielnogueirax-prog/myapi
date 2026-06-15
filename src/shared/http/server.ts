07808e53b6e5c6f88bba396eaf77fd2fbd2d6f6f
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-15 20:21:22 -0300

    Error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,13 +1,31 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import { routes } from '../routes';
+import { AppError } from '../erros/AppError';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use(routes);
+// middleware de tratamento de erros
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.error(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Erro Interno Server.',
+    });
+  },
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,10 @@
+import { AppError } from '../erros/AppError';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
+  throw new Error('Erro acesso negado');
   return response.json({ message: 'Hello World!' });
 });
 
