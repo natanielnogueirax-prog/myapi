2ad3d516da5d6259e450096d8e4e0ef885017351
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-15 22:02:46 -0300

    add roles repository

---
+++ src/roles/repositories/RolesRepository.ts
@@ -0,0 +1,30 @@
+import { Role } from '@roles/entities/Role';
+
+// DTO - Data Transfer Object - é um padrão de projeto usado para transferir dados entre camadas ou sistemas.
+// Ele é uma representação simplificada de um objeto complexo, contendo apenas os dados necessários para a transferência.
+// O DTO é usado para evitar o acoplamento entre as camadas e para melhorar a performance, reduzindo a quantidade de dados transferidos.
+// Ele pode ser usado em conjunto com padrões como o Repository e o Service para organizar a lógica de negócios e a persistência de dados em uma aplicação.
+// Rota role - POST /roles - criar um novo role objeto tem id que vem do uuid, name onde é  usado o DTO e created_at que é a data de criação do role
+type CreateRoleDTO = {
+  name: string;
+};
+
+export class RolesRepository {
+  private roles: Role[];
+
+  constructor() {
+    this.roles = [];
+  }
+
+  create({ name }: CreateRoleDTO) {
+    const role = new Role();
+
+    Object.assign(role, {
+      name,
+      created_at: new Date(),
+    });
+
+    this.roles.push(role);
+    return role;
+  }
+}
