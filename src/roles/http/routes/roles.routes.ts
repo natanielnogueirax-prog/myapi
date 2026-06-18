11ef37d1167ee735852379f82cca37393aa55a4a
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-17 21:24:13 -0300

    create list controller

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,10 +1,9 @@
-import { RolesRepository } from '@roles/repositories/RolesRepository';
 import { createRoleController } from '@roles/useCases/createRole';
+import { listRolesController } from '@roles/useCases/listRoles';
 import { Router } from 'express';
 
 // instancia do routes
 const rolesRouter = Router();
-const rolesRepository = new RolesRepository();
 
 // Rota de post para criar, chama o created do repository passando o name do body da requisição,
 // e retorna o role criado com status 201
@@ -14,9 +13,7 @@ rolesRouter.post('/', (request, response) => {
 
 // Rota get lista as informações usando o find all
 rolesRouter.get('/', (request, response) => {
-  const roles = rolesRepository.findAll();
-
-  return response.json(roles);
+  return listRolesController.handle(request, response);
 });
 
 export { rolesRouter };
