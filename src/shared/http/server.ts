63a96ab5276bd96d61abcb493ab5bed6f4fe5e60
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-18 21:09:25 -0300

    install typeorm, reflec metadata e sqlite

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import SwaggerUi from 'swagger-ui-express';
