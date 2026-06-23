b1c23fb3da0e7253888cbfc47754ebb0ff8867b0
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-22 22:17:53 -0300

    refactor role entitie

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,8 +1,17 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
 
+//@ são decorators no typeorm, ver na documentação do typeorm entities special columns
+// tem que habilitar decorators no ts config json
+@Entity('roles')
 export class Role {
-  id: string;
+  @PrimaryColumn('uuid')
+  id?: string;
+
+  @Column('varchar', { length: 255 })
   name: string;
+
+  @CreateDateColumn({ type: 'datetime' })
   created_at: Date;
 
   constructor() {
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1782174927106 } from './migrations/1782174927106-CreateRolesTable';
+import { Role } from '@roles/entities/Role';
 
 export const dataSource = new DataSource({
   type: 'better-sqlite3',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1782174927106],
 });
