e3f7f2175f78facb8f187f6005461e658f2d3df6
Natãniel João Nogueira <natanielnogueirax@icloud.com>
2026-06-22 21:57:19 -0300

    Create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm';
+import { CreateRolesTable1782174927106 } from './migrations/1782174927106-CreateRolesTable';
 
 export const dataSource = new DataSource({
   type: 'better-sqlite3',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1782174927106],
 });
